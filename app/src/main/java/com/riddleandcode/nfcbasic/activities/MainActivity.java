64dbcc312f3b36b70bc702a45ab224ce670ed58b
Elizabeth Navarro Salva <elizabeth.navarro.1612@gmail.com>
2017-01-27 14:25:07 +0100

    Basic NFC reader and address checking

---
+++ app/src/main/java/com/riddleandcode/nfcbasic/activities/MainActivity.java
@@ -0,0 +1,202 @@
+package com.riddleandcode.nfcbasic.activities;
+
+import com.riddleandcode.nfcbasic.R;
+import com.riddleandcode.nfcbasic.managers.TagManager;
+import com.riddleandcode.nfcbasic.models.Balance;
+import com.riddleandcode.nfcbasic.utils.Constants;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static String API_URL = "https://chain.so/api/v2";
+    private static String GET_BALANCE_URL = "/get_address_balance";
+    private static String NETWORK = "/BTCTEST";
+
+    private TextView mTvReadTag;
+    private LinearLayout mInfoLayout;
+    private TextView mNetwork;
+    private TextView mConfirmedBalance;
+    private TextView mUnconfirmedBalance;
+
+    private ProgressBar mProgressBar;
+
+    private TagManager mTagManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+
+        mTagManager = new TagManager(this);
+        mTvReadTag.setText(mTagManager.isAvailable() ? R.string.tag_available : R.string.tag_unavailable);
+    }
+
+    private void bindViews(){
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+        mTvReadTag = (TextView)findViewById(R.id.info_test);
+        mInfoLayout = (LinearLayout)findViewById(R.id.info_layout);
+        mNetwork = (TextView)findViewById(R.id.tv_network);
+        mConfirmedBalance = (TextView)findViewById(R.id.tv_confirmed_balance);
+        mUnconfirmedBalance = (TextView)findViewById(R.id.tv_unconfirmed_balance);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableForegroundDispatchSystem();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        enableForegroundDispatchSystem();
+    }
+
+    private void enableForegroundDispatchSystem() {
+        Intent intent = new Intent(this, MainActivity.class).addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        IntentFilter[] intentFilters = new IntentFilter[] {};
+
+        mTagManager.getAdapter().enableForegroundDispatch(this, pendingIntent, intentFilters, null);
+    }
+
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if(intent.hasExtra(NfcAdapter.EXTRA_TAG)) {
+            Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+            try {
+                mTagManager.ntagInit(tagFromIntent);
+                mTagManager.ntagConnect();
+                mTagManager.ntagGetVersion();
+
+                do {
+                    boolean boolVar = true;
+                } while (mTagManager.ntagGetNsReg(0x00,0) !=  1);
+
+                mTagManager.ntagSectorSelect((byte) 0x00);
+                mTagManager.ntagRead((byte) 0x04);
+                fetchData();
+
+                mTagManager.setTimeout(100);
+                mTagManager.ntagClose();
+            } catch (Exception e) {
+                Toast.makeText(this, "Tag reading Error: ", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+    private void fetchData(){
+        new RetrieveFeedTask().execute();
+    }
+
+    private void disableForegroundDispatchSystem() {
+        mTagManager.getAdapter().disableForegroundDispatch(this);
+    }
+
+    private class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+        protected void onPreExecute() {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        protected String doInBackground(Void... urls) {
+//            String address = bytesToHex(answer);
+            try {
+                //Hacked address until the device read a correct one from the antenna
+                URL url = getUrlWithParams("mobyyYFM7HafjFBtca9PAyN7TUAE5uiZFf");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e(TAG, e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            mProgressBar.setVisibility(View.GONE);
+            if(response != null){
+                handleResponse(response);
+            }
+        }
+    }
+
+    private void handleResponse(String response){
+        try {
+            JSONObject data = new JSONObject(response);
+            if(data.getString(Constants.JSON_STATUS).equals(Constants.JSON_SUCCESS)){
+                Balance balance = new Balance(data.getJSONObject(Constants.JSON_DATA));
+                setBalanceInfo(balance);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setBalanceInfo(Balance balance){
+        mTvReadTag.setVisibility(View.GONE);
+        mInfoLayout.setVisibility(View.VISIBLE);
+        mNetwork.setText(getString(R.string.network,balance.getNetwork()));
+        mConfirmedBalance.setText(getString(R.string.confirmed_balance,balance.getConfirmedBalance()));
+        mUnconfirmedBalance.setText(getString(R.string.unconfirmed_balance,balance.getUnconfirmedBalance()));
+    }
+
+    private URL getUrlWithParams(String address){
+        try {
+            return new URL(getBalanceUrl() + NETWORK + "/" + address);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getBalanceUrl(){
+        return API_URL + GET_BALANCE_URL;
+    }
+
+}
+
---
+++ app/src/main/java/com/riddleandcode/nfcbasic/managers/TagManager.java
@@ -0,0 +1,165 @@
+package com.riddleandcode.nfcbasic.managers;
+
+import android.content.Context;
+import android.nfc.FormatException;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcA;
+
+import java.io.IOException;
+
+/**
+ * Created by Eyliss on 1/27/17.
+ */
+
+public class TagManager {
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    private NfcAdapter nfcAdapter;
+    private NfcA nfca;
+
+    private byte current_sec = 0;
+    private int sector_select_timeout = 0;
+    private final int timeout = 20;
+    private byte[] answer;
+    private byte[] command;
+
+    public TagManager(Context context){
+        nfcAdapter = NfcAdapter.getDefaultAdapter(context);
+    }
+
+    public boolean isAvailable(){
+        return nfcAdapter != null;
+    }
+
+    public NfcAdapter getAdapter(){
+        return nfcAdapter;
+    }
+
+    /** Functions related with the tag interaction **/
+
+    //special read command
+    public int ntagGetNsReg(int regAddr, int pos) throws IOException, FormatException {
+        ntagSectorSelect((byte) 0x03);
+        answer = ntagRead((byte) 0xF8);
+
+        return (int) (answer[regAddr] >> pos) & 1;
+    }
+
+    public void ntagInit(Tag tag) {
+        nfca = NfcA.get(tag);
+        sector_select_timeout = 20;
+        nfca.setTimeout(20);
+        current_sec = 0;
+    }
+
+    public void ntagClose() throws IOException {
+        nfca.close();
+        current_sec = 0;
+    }
+
+    public void ntagConnect() throws IOException {
+        nfca.connect();
+        current_sec = 0;
+    }
+
+    public boolean ntagIsConnected() {
+        return nfca.isConnected();
+    }
+
+    public byte[] ntagGetLastCommand() {
+        return command;
+    }
+
+    public byte[] ntagGetLastAnswer() {
+        return answer;
+    }
+
+    public void ntagSectorSelect(byte sector) throws IOException, FormatException {
+        if(current_sec != sector) {
+            command = new byte[2];
+            command[0] = -62;
+            command[1] = -1;
+            nfca.transceive(this.command);
+            command = new byte[4];
+            command[0] = sector;
+            command[1] = 0;
+            command[2] = 0;
+            command[3] = 0;
+            nfca.setTimeout(sector_select_timeout);
+
+            try {
+                nfca.transceive(this.command);
+            } catch (IOException var3) {
+                var3.printStackTrace();
+            }
+
+            nfca.setTimeout(20);
+            current_sec = sector;
+        }
+    }
+
+    public void ntagFastWrite(byte[] data, byte startAddr, byte endAddr) throws IOException, FormatException {
+        answer = new byte[0];
+        command = new byte[3 + data.length];
+        command[0] = -90;
+        command[1] = startAddr;
+        command[2] = endAddr;
+        System.arraycopy(data, 0, command, 3, data.length);
+        nfca.setTimeout(500);
+        nfca.transceive(command);
+        nfca.setTimeout(20);
+    }
+
+    public void ntagWrite(byte[] data, byte blockNr) throws IOException, FormatException {
+        answer = new byte[0];
+        command = new byte[6];
+        command[0] = -94;
+        command[1] = blockNr;
+        command[2] = data[0];
+        command[3] = data[1];
+        command[4] = data[2];
+        command[5] = data[3];
+        nfca.transceive(command);
+    }
+
+    public byte[] ntagFastRead(byte startAddr, byte endAddr) throws IOException, FormatException {
+        command = new byte[3];
+        command[0] = 58;
+        command[1] = startAddr;
+        command[2] = endAddr;
+        nfca.setTimeout(500);
+        answer = nfca.transceive(command);
+        nfca.setTimeout(20);
+        return answer;
+    }
+
+    public byte[] ntagRead(byte blockNr) throws IOException, FormatException {
+        command = new byte[2];
+        command[0] = 48;
+        command[1] = blockNr;
+        answer = nfca.transceive(command);
+        return answer;
+    }
+
+    public byte[] ntagGetVersion() throws IOException {
+        command = new byte[1];
+        command[0] = 96;
+        answer = nfca.transceive(command);
+        return answer;
+    }
+
+    public void setTimeout(int timeout){
+        nfca.setTimeout(timeout);
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
